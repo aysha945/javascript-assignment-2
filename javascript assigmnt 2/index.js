573351afa11e490cbb966a718e19d2ca1f43aaed
aysha945 <76861040+aysha945@users.noreply.github.com>
2021-08-28 22:50:03 +0530

    day 2 assignment

---
+++ javascript assigmnt 2/index.js
@@ -0,0 +1,26 @@
+let food=[
+    {
+        name:"noodles",
+        price:50,
+        quantity:"10g"
+
+    }
+    ,
+    {
+        name:"plain rice",
+        price:70,
+        quantity:"25g"
+    }
+    ,
+    {
+        name:"biriyani",
+        price:150,
+        quantity:"500g"
+    }
+
+]
+ for(let num=0;num<=2;num++)
+ {
+    console.log(food[num]);
+    
+ }
